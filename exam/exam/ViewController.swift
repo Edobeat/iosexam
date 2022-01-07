370fdd1837164fa4a92df71a2bd0e2e3c5255277
Edis Hasanov <edishasanov@abv.bg>
2022-01-07 09:08:56 -0800

    Added table view

--- exam/exam/ViewController.swift
+++ exam/exam/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
+    private lazy var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        title = "Books"
+        setUpTableView()
     }
-
-
 }
 
+private extension MainViewController {
+    func setUpTableView() {
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            fatalError("opa")
+        }
+        cell.textLabel?.text = "Proba \(indexPath.row)"
+        return cell
+    }
+}
